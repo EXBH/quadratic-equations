6de9d952b8ab4a29239411670c47d5c59cbe9042
EXBH <yasimko2011@gmail.com>
2025-12-14 17:54:04 +0600

    Enhance quadratic equation solver functionality
    Refactor quadratic equation solver to handle cases with equal roots and improve output formatting.

--- quadratic equations.py
+++ quadratic equations.py
@@ -15,38 +15,39 @@ else:
         print(f"Discriminant not equal type INT")
         print(f"answer: x1 = {-b} - √{Discriminant} / {a*2}")
         print(f"answer: x2 = {-b} + √{Discriminant} / {a*2}")
-    else:    
+    else:
         x1f1 = int(-b - Discriminant**0.5)
         x1f2 = int(2 * a)
 
-        frac = Fraction(x1f1, x1f2)
-        sign = -1 if frac < 0 else 1
-        abs_frac = abs(frac)
+        frac1 = Fraction(x1f1, x1f2)
+        sign1 = -1 if frac1 < 0 else 1
+        abs_frac = abs(frac1)
 
-        whole = abs_frac.numerator // abs_frac.denominator
-        rest = abs_frac.numerator % abs_frac.denominator
-
-        if rest == 0:
-            print("x1 =", sign * whole)
-        elif whole > 0:
-            print(f"x1 = {sign * whole} {rest}/{abs_frac.denominator}")
-        else:
-            print(f"x1 = {frac}")
+        whole1 = abs_frac.numerator // abs_frac.denominator
+        rest1 = abs_frac.numerator % abs_frac.denominator
 
         x2f1 = int(-b + Discriminant**0.5)
         x2f2 = int(2 * a)
 
-        frac = Fraction(x2f1, x2f2)
-        sign = -1 if frac < 0 else 1
-        abs_frac = abs(frac)
+        frac2 = Fraction(x2f1, x2f2)
+        sign2 = -1 if frac2 < 0 else 1
+        abs_frac = abs(frac2)
 
-        whole = abs_frac.numerator // abs_frac.denominator
-        rest = abs_frac.numerator % abs_frac.denominator
-
-        if rest == 0:
-            print("x2 =", sign * whole)
-        elif whole > 0:
-            print(f"x2 = {sign * whole} {rest}/{abs_frac.denominator}")
+        whole2 = abs_frac.numerator // abs_frac.denominator
+        rest2 = abs_frac.numerator % abs_frac.denominator
+        if frac1 == frac2 and Discriminant == 0:
+            print(f"you have one x. Your x = {frac1}")    
         else:
-            print(f"x2 = {frac}")
+            if rest1 == 0:
+                print("x1 =", sign1 * whole1)
+            elif whole1 > 0:
+                print(f"x1 = {sign1 * whole1} {rest1}/{abs_frac.denominator}")
+            else:
+                print(f"x1 = {frac1}")
+            if rest2 == 0:
+                print("x2 =", sign2 * whole2)
+            elif whole2 > 0:
+                print(f"x2 = {sign2 * whole2} {rest2}/{abs_frac.denominator}")
+            else:
+                print(f"x2 = {frac2}")
 
